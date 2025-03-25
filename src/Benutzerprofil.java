3fdec875b6c6d694239858b1aaeda3c53ede2eef
kosychsa <kosych.sascha@gmail.com>
2025-03-25 13:14:51 +0100

    anlegen

--- src/Benutzerprofil.java
+++ src/Benutzerprofil.java
@@ -1,3 +1,11 @@
 public class Benutzerprofil {
+    private String benutzername;
+
+    Benutzerprofil(String login, String pw){}
+
+    public String getBenutzername(){
+        return benutzername;
+    }
+
 
 }
