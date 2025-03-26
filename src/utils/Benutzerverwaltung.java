42f8d1b7eddaa63334de76b0d0abfeb29dcaadcf
kosychsa <kosych.sascha@gmail.com>
2025-03-26 11:24:25 +0100

    anlegen

---
+++ src/utils/Benutzerverwaltung.java
@@ -0,0 +1,16 @@
+package utils;
+public class Benutzerverwaltung {
+
+    public Benutzerverwaltung(){
+
+    }
+    public void neuenBenutzerAnlegen(){
+
+    }
+    public void nutzerLoeschen(){
+
+    }
+    public void profilVoranden(){
+
+    }
+}
