5f0dad73568d8267be318df186fa0793ed26c1fa
kosychsa <kosych.sascha@gmail.com>
2025-03-26 11:04:28 +0100

    anlegen

--- src/Benutzerprofil.java
+++ src/Benutzerprofil.java
@@ -1,10 +1,25 @@
-public class Benutzerprofil {
+import utils.ComparableContent;
+
+public class Benutzerprofil implements ComparableContent {
     private String benutzername;
 
-    Benutzerprofil(String login, String pw){}
+    Benutzerprofil(String login, String pw) {
+    }
 
-    public String getBenutzername(){
+    public String getBenutzername() {
         return benutzername;
     }
 
+    public boolean isLess(Benutzerprofil pContent) {
+        return this.benutzername.compareTo(pContent.getBenutzername()) < 0;
+    }
+
+    public boolean isEqual(Benutzerprofil pContent) {
+       return this.benutzername.equals(pContent.getBenutzername());
+    }
+
+    public boolean isGreater(Benutzerprofil pContent) {
+        return this.benutzername.compareTo(pContent.getBenutzername()) > 0;
+    }
 }
+
